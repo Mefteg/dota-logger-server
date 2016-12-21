04b82f10749790250fabb8a1a6bdc5e9fe27c456
Unknown <gimenez.tom@gmail.com>
2016-12-20 21:11:31 -0500

    Get info for Heroes

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var PORT = (process.env.PORT || 5000);
 var TAG_START = "##START";
 var TAG_END   = "##END";
 
+var HEROES = {};
+GetHeroesInformation();
+
 var server = restify.createServer();
 server.get('/info/:user_id', HandleInfoRequest);
 server.listen(PORT, function() {
@@ -15,29 +18,31 @@ server.listen(PORT, function() {
 
 function HandleInfoRequest(_req, _res, _next) {
   try {
+    // if we don't have heroes information
+    if (HEROES.length == 0) {
+      // get them
+      GetHeroesInformation();
+    }
+
+    var localData = {};
     var userID = _req.params.user_id;
     if (userID) {
-
-
-      var url = "http://api.opendota.com/api/players/" + userID + "/wl";
-      http.get(url, function(res) {
-        if (res.statusCode != 200) {
+      GetWinLose(userID, function(error, data) {
+        if (error) {
           SendError(_res);
         }
         else {
-          var rawData = '';
-          res.setEncoding('utf8');
-          res.on('data', (chunk) => rawData += chunk);
-          res.on('end', function() {
-            var parsedData = JSON.parse(rawData);
+          localData.win = data.win;
+          localData.lose = data.lose;
+
+          GetHeroes(userID, function(error, data) {
+            localData.heroes = data.heroes;
 
             var text = TAG_START + '\n';
-            text += CreateTextToSend(parsedData) + '\n';
+            text += CreateTextToSend(localData) + '\n';
             text += TAG_END;
 
             _res.setHeader('Content-Type', 'text/plain');
-            //_res.setHeader('Connection', 'keep-alive');
-            //_res.write(userID, "utf-8");
             _res.write(text);
             console.log("SENDING...");
             _res.end(function() {
@@ -63,8 +68,100 @@ function SendError(_res) {
   });
 }
 
+function GetWinLose(_userID, _callback) {
+  var url = "http://api.opendota.com/api/players/" + _userID + "/wl";
+  http.get(url, function(res) {
+    if (res.statusCode != 200) {
+      _callback("Error", null);
+    }
+    else {
+      var rawData = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => rawData += chunk);
+      res.on('end', function() {
+        var parsedData = JSON.parse(rawData);
+
+        var data = {
+          win: parsedData.win,
+          lose: parsedData.lose
+        };
+
+        _callback(null, data);
+      });
+    }
+  });
+}
+
+function GetHeroes(_userID, _callback) {
+  var url = "http://api.opendota.com/api/players/" + _userID + "/heroes";
+  http.get(url, function(res) {
+    if (res.statusCode != 200) {
+      _callback("Error", null);
+    }
+    else {
+      var rawData = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => rawData += chunk);
+      res.on('end', function() {
+        var parsedData = JSON.parse(rawData);
+
+        var heroes = [];
+        for (var i=0; i<3; ++i) {
+          var hero = parsedData[i];
+          heroes.push({
+            hero_id: hero.hero_id,
+            games: hero.games,
+            win: hero.win
+          });
+        }
+
+        var data = {
+          heroes: heroes
+        };
+
+        _callback(null, data);
+      });
+    }
+  });
+}
+
 function CreateTextToSend(_data) {
   var text = "W/L: " + _data.win + "/" + _data.lose + '\n';
+  text += "===\n";
+
+  var nbHeroes = _data.heroes.length;
+  for (var i=0; i<nbHeroes; ++i) {
+    var hero = _data.heroes[i];
+
+    var name = HEROES[hero.hero_id].localized_name;
+    var lose = hero.games - hero.win;
+    var ratio = Math.floor((hero.win / hero.games) * 100);
+    text += name + ": " + hero.games + " (" + hero.win + '/' + lose + " | " + ratio + "%)\n";
+  }
 
   return text;
+}
+
+function GetHeroesInformation() {
+  var url = "http://api.opendota.com/api/heroes";
+  http.get(url, function(res) {
+    if (res.statusCode != 200) {
+      console.log("No information on heroes.");
+    }
+    else {
+      var rawData = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => rawData += chunk);
+      res.on('end', function() {
+        var parsedData = JSON.parse(rawData);
+
+        HEROES = {};
+        var nbHeroes = parsedData.length;
+        for (var i=0; i<nbHeroes; ++i) {
+          var hero = parsedData[i];
+          HEROES[hero.id] = hero;
+        }
+      });
+    }
+  });
 }
